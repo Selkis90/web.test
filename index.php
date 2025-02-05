6b92412da21207a9c96de539eca531883b398648
Selkis90 <selkis@gmail.com>
2025-02-04 22:31:35 -0500

    se mejora la interfas del login

--- index.php
+++ index.php
@@ -9,21 +9,19 @@ require_once './parametros.php'; // Incluye la configuración global
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/login.css">
     <title>Inicio</title>
 </head>
 
 <body>
-    <div class="container mt-4">
-        <h1 class="text-center mb-4">Bienvenido a la aplicación</h1>
-        <div class="d-flex justify-content-center">
-            <!-- Enlaces usando la constante _URL para mantener rutas dinámicas -->
+    <div class="login-container">
+        <h1>Bienvenido a la aplicación</h1>
+        <p>Gestiona tu cuenta de forma sencilla</p>
+        <div class="d-flex justify-content-center mt-4">
             <a href="<?php echo _URL; ?>php/login.php" class="btn btn-secondary">Iniciar Sesión</a>
-            <hr>
-            <a href="<?php echo _URL; ?>php/register.php" class="btn btn-primary me-2">Registrarse</a>
-
+            <a href="<?php echo _URL; ?>php/register.php" class="btn btn-primary">Registrarse</a>
         </div>
     </div>
-
 </body>
 
 </html>
