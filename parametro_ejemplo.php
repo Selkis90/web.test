1a6b60d26cd85fdd2784d1df9d0554a27f6fa08c
Selkis <Selkis90@gmail.com>
2025-06-01 19:12:35 -0500

    se crea el archivo parametros para realizar la conexion a la base de datos

---
+++ parametro_ejemplo.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Haga una copia de este archivo y renómbrelo a 'parametros.php'
+ */
+
+// Definición de constantes para la conexión a la base de datos
+define('_HOST', 'localhost');
+define('_USER', 'usruario_de_su_base_de_datos');
+define('_PASSWORD', 'password_de_su_base_de_datos');
+define('_BD_NAME', 'nombre_de_su_base_de_datos');
+
+// URL de la aplicación
+define('_APPLICATION_NAME', 'healthcare');
+define('_URL', 'http://web.test/'); // Cambiar por la URL de su proyecto: OJO: Siempre terminar con una barra al final /
